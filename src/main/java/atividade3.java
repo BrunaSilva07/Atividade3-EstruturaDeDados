4dc522097a7ec43ff1911ef1a29331f284bd0453
Bruna Silva <bruna-m.silva@hotmail.com>
2020-06-01 15:01:27 -0300

    atualização 01-06-2020

--- src/main/java/atividade3.java
+++ src/main/java/atividade3.java
@@ -1,5 +1,4 @@
 import org.springframework.boot.SpringBootConfiguration;
-
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 
@@ -9,15 +8,14 @@ public class atividade3 {
     public static void main(String[] args) {
 
         System.out.println("===============================Criando a primeira Lista=================================");
+        System.out.println();
         //Criando a lista com 5 células
         ArrayList<Integer> lista = new ArrayList<>(5);
 
         //Inserindo os numeros 1,2,3,4,5 na lista
-        lista.add(1);
-        lista.add(2);
-        lista.add(3);
-        lista.add(4);
-        lista.add(5);
+        for (int numero = 1; numero <= 5; numero++) {
+            lista.add(numero);
+        }
 
         System.out.println("=-=-=-=-=-Adicionado na Lista-=-=-=-=-=");
         System.out.println(lista);
@@ -28,8 +26,8 @@ public class atividade3 {
 
         //Removendo os dados das celulas iniciais da lista e incluindo na pilha
 
-        while(!lista.isEmpty()) {
-            for(int n = 1; n <= 5; n++){
+        while (!lista.isEmpty()) {
+            for (int n = 1; n <= 5; n++) {
                 pilha.push(n);
                 int elemento = lista.remove(0);
 
@@ -48,8 +46,8 @@ public class atividade3 {
         ArrayDeque<Integer> fila = new ArrayDeque<>(10);
 
         //Removendo os dados da pilha e incluindo na fila
-        while(!pilha.isEmpty()) {
-            for(int n = 1; n <= 5 ; n++) {
+        while (!pilha.isEmpty()) {
+            for (int n = 1; n <= 5; n++) {
                 fila.add(n);
                 int elemento = pilha.pop();
                 System.out.println("=-=-=-=-=-Esvaziando pilha-=-=-=-=-=");
@@ -63,14 +61,13 @@ public class atividade3 {
         }
 
         System.out.println("================================Criando a segunda lista==============================");
+        System.out.println();
         ArrayList<Integer> lista2 = new ArrayList<>(5);
 
         //Inserindo os numeros 6,7,8,9 e 10 na lista
-        lista2.add(6);
-        lista2.add(7);
-        lista2.add(8);
-        lista2.add(9);
-        lista2.add(10);
+        for (int numero = 1; numero <= 5; numero++) {
+            lista2.add(numero);
+        }
 
         System.out.println("=-=-=-=-=-Adicionado na Lista2-=-=-=-=-=");
         System.out.println(lista2);
@@ -81,8 +78,8 @@ public class atividade3 {
 
         //Removendo os dados das celulas iniciais da lista2 e incluindo na pilha2
 
-        while(!lista2.isEmpty()) {
-            for(int n = 6; n <= 10; n++){
+        while (!lista2.isEmpty()) {
+            for (int n = 6; n <= 10; n++) {
                 pilha2.push(n);
                 int elemento = lista2.remove(0);
 
@@ -97,13 +94,13 @@ public class atividade3 {
         System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
 
         //Removendo os dados da pilha2 e incluindo na fila
-        while(!pilha2.isEmpty()) {
-            for(int n = 6; n <= 10 ; n++) {
+        while (!pilha2.isEmpty()) {
+            for (int n = 6; n <= 10; n++) {
                 fila.add(n);
                 int elemento = pilha2.pop();
                 System.out.println("=-=-=-=-=-Esvaziando pilha2-=-=-=-=-=");
                 System.out.println(pilha2);
-                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
             }
 
             System.out.println("\n=-=-=-=-=-Adicionada na fila-=-=-=-=-=");
